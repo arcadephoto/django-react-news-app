df9322ab8d48d74560caf8268997151d44eacea1
arcadephoto <>
2021-03-01 14:37:51 -0500

    adds static

---
+++ frontend/static/src/App.js
@@ -0,0 +1,14 @@
+import React, {Component} from 'react'
+import './App.css';
+
+class App extends Component {
+  render(){
+
+      return (
+    <div className="App">
+      Words n stuff
+    </div>
+  );
+}
+}
+export default App;
